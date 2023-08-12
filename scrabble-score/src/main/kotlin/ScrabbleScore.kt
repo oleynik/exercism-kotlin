03f74ba0110c05478ec5bf61cc20f07ab1e82428
Alex Oliinyk <alex.p.oliinyk@gmail.com>
2023-08-12 15:01:45 +0200

    Implemented scrabble-score exercise.

---
+++ scrabble-score/src/main/kotlin/ScrabbleScore.kt
@@ -0,0 +1,20 @@
+object ScrabbleScore {
+
+    private fun scoreLetter(c: Char) = when (c) {
+        'a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't' -> 1
+        'd', 'g' -> 2
+        'b', 'c', 'm', 'p' -> 3
+        'f', 'h', 'v', 'w', 'y' -> 4
+        'k' -> 5
+        'j', 'x' -> 8
+        'q', 'z' -> 10
+        else -> 0
+    }
+
+    fun scoreWord(word: String): Int {
+        return word
+            .lowercase()
+            .toCharArray()
+            .sumOf { scoreLetter(it) }
+    }
+}
