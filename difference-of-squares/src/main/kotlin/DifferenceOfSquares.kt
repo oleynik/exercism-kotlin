38c3921b135bfe34098f59d70375ab50029d2230
Alex Oliinyk <alex.p.oliinyk@gmail.com>
2023-08-12 15:27:41 +0200

    Implemented difference-of-square exercise.

---
+++ difference-of-squares/src/main/kotlin/DifferenceOfSquares.kt
@@ -0,0 +1,16 @@
+class Squares(private val n: Int) {
+
+    fun sumOfSquares(): Int {
+        return (1..n)
+            .sumOf { it * it }
+    }
+
+    fun squareOfSum(): Int {
+        val sum = (1..n).sum()
+        return sum*sum
+    }
+
+    fun difference(): Int {
+        return squareOfSum() - sumOfSquares()
+    }
+}
